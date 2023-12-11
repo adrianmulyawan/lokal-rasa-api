52a8b72d09ac97173594bf76c3ee978416bae213
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-12-11 20:16:28 +0700

    add route and controller to edit and delete region

--- src/controllers/region.controller.ts
+++ src/controllers/region.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
-import slug from 'slug'
+import slugLib from 'slug'
 
 const prisma = new PrismaClient();
 
@@ -55,7 +55,7 @@ export const addNewRegion =async (req: Request, res: Response) => {
     const region = await prisma.region.create({
       data: {
         region_name: region_name,
-        region_slug: slug(region_name)
+        region_slug: slugLib(region_name)
       }
     });
 
@@ -107,6 +107,99 @@ export const showDetailRegion =async (req: Request, res: Response) => {
       statusCode: 200,
       message: "Region is Found!",
       data: region
+    });
+
+  } catch (error: any) {
+    return res.status(400).json({
+      status: "Failed",
+      statusCode: 400,
+      message: `Something Error in ${error.message}`
+    });
+  }
+};
+
+export const updateRegion = async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  const { region_name } = req.body;
+
+  try {
+    if (!region_name) {
+      return res.status(400).json({
+        status: "Failed",
+        statusCode: 400,
+        message: "Region Name is Required!"
+      });
+    }
+
+    const region = await prisma.region.findFirst({
+      where: {
+        region_slug: slug
+      }
+    });
+
+    if (!region) {
+      return res.status(404).json({
+        status: "Failed",
+        statusCode: 404,
+        message: "Region is Not Found!"
+      });
+    } else {
+      const update = await prisma.region.update({
+        where: {
+          region_slug: String(slug),
+          id: region.id
+        },
+        data: {
+          region_name: region_name || region.region_name,
+          region_slug: slugLib(region_name) || region.region_slug
+        }
+      });
+
+      return res.status(200).json({
+        status: "Success",
+        statusCode: 200,
+        data: update
+      });
+    }
+
+  } catch (error: any) {
+    return res.status(400).json({
+      status: "Failed",
+      statusCode: 400,
+      message: `Something Error in ${error.message}`
+    });
+  }
+};
+
+export const deleteRegion = async (req: Request, res: Response) => {
+  const { slug } = req.params;
+
+  try {
+    const region = await prisma.region.findFirst({
+      where: {
+        region_slug: slug
+      }
+    });
+
+    if (!region) {
+      return res.status(404).json({
+        status: "Failed",
+        statusCode: 404,
+        message: "Region is Not Found!"
+      });
+    }
+
+    await prisma.region.delete({
+      where: {
+        region_slug: region.region_slug,
+        id: region.id
+      }
+    });
+
+    return res.status(200).json({
+      status: "Failed",
+      statusCode: 200,
+      message: "Success Delete Region!"
     });
 
   } catch (error: any) {
--- src/routes/region.route.ts
+++ src/routes/region.route.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 
-import { showRegion, addNewRegion, showDetailRegion } from '../controllers/region.controller';
+import { 
+  showRegion, 
+  addNewRegion, 
+  showDetailRegion,
+  updateRegion,
+  deleteRegion
+} from '../controllers/region.controller';
 
 const router = express.Router();
 
 router.get('/api/v1/region/show', showRegion);
 router.post('/api/v1/region/add', addNewRegion);
 router.get('/api/v1/region/detail/:slug', showDetailRegion);
+router.put('/api/v1/region/edit/:slug', updateRegion);
+router.delete('/api/v1/region/delete/:slug', deleteRegion);
 
 export default router;
