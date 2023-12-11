c7f7098277852be323e5877ee4a6d271e15f6124
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-12-11 15:05:31 +0700

    import region route

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import express, {Request, Response} from 'express';
 import 'dotenv/config';
 import 'http-errors';
 
+import RegionRoute from './routes/region.route';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(RegionRoute);
 
 app.use((req: Request, res: Response, next: Function) => {
   res.status(404).json({
