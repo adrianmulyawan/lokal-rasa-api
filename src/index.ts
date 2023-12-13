6acb0b196c0c433a06e62ef74120517590013d30
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-12-13 20:43:01 +0700

    import express static in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import 'http-errors';
+import path from 'path';
 
 import RegionRoute from './routes/region.route';
 import ProvinceRoute from './routes/province.route';
 import CategoryRoute from './routes/category.route';
+import FoodRoute from './routes/food.route';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,11 +14,16 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Middleware untuk menangani file statis
+app.use('/images', express.static(path.join(__dirname, '../public/images')));
+
 app.use(RegionRoute);
 app.use(ProvinceRoute);
 app.use(CategoryRoute);
+app.use(FoodRoute);
 
 app.use((req: Request, res: Response, next: Function) => {
+  console.log('404 handler:', req.url);
   res.status(404).json({
     status: "Failed",
     statusCode: 404,
@@ -26,4 +33,4 @@ app.use((req: Request, res: Response, next: Function) => {
 
 app.listen(port, () => {
   console.info(`Express Run in Port ${port}`);
-})
+});
